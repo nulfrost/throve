920d60b3d4a3ca86102d24ab0833dc0a9766be4e
nulfrost <khadane.miller@gmail.com>
2024-11-25 21:42:36 -0500

    feat: working on database schema

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,6 +1,45 @@
-import { pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { nanoid } from "nanoid";
 
-export const statusTable = pgTable("status_table", {
+export const users = pgTable("users", {
+  id: text("id")
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
+  did: varchar({ length: 255 }).notNull(),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+  updated_at: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const feeds = pgTable("feeds", {
+  id: text("id")
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
+  name: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 255 }),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+  updated_at: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const links = pgTable("links", {
+  id: text("id")
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
+  url: text("url").notNull(),
+  position: integer(),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+  updated_at: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const status = pgTable("status", {
   uri: varchar({ length: 255 }).primaryKey().unique(),
   author_did: varchar({ length: 255 }).notNull(),
   status: varchar({ length: 255 }).notNull(),
@@ -8,12 +47,12 @@ export const statusTable = pgTable("status_table", {
   indexed_at: varchar({ length: 255 }).notNull(),
 });
 
-export const authSessionTable = pgTable("auth_session_table", {
+export const sessions = pgTable("sessions", {
   key: varchar({ length: 255 }).primaryKey().unique(),
   session: varchar({ length: 255 }).notNull(),
 });
 
-export const authStateTable = pgTable("auth_state_table", {
+export const states = pgTable("states", {
   key: varchar({ length: 255 }).primaryKey().unique(),
   state: varchar({ length: 255 }).notNull(),
 });
